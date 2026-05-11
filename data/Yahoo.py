9083c50ecb8a662fb3ed9576ac9c5fa73db65d1f
Welkie <lamhuynhkhang281@gmail.com>
2026-05-11 17:36:14 +0700

    wsz = 350 for yahoo

--- data/Yahoo.py
+++ data/Yahoo.py
@@ -42,7 +42,7 @@ class Yahoo(Dataset):
 
         self.data = np.asarray(data)
         self.targets = np.asarray(label)
-        wsz, stride = 250, 1
+        wsz, stride = 350, 1
 
         if len(self.data) < wsz:
             raise ValueError(
